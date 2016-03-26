9588bd52ed5e68b89489905769989b2a855d042a
anthonybrown <tony@tonybrown.me>
2016-03-26 12:01:13 -0400

    setup token and persistance

---
+++ src/models/me.js
@@ -0,0 +1,22 @@
+import Model from 'ampersand-model'
+
+export default Model.extend({
+	initialize () {
+		this.token = window.localStorage.token
+		this.on('change:token', this.onChangeToken)
+	},
+
+	props: {
+		id         : 'number',
+		login      : 'string',
+		avatar_url : 'string'
+	},
+
+	session: {
+		token: 'string'
+	},
+
+	onChangeToken () {
+		window.localStorage.token = this.token
+	}
+})
